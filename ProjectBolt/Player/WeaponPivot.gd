119e539867d99d9d180006b3f8f77173cb5cc824
Philipp <philippbecker1@web.de>
2023-01-20 03:40:53 +0100

    added jetpack and rotation fix

--- ProjectBolt/Player/WeaponPivot.gd
+++ ProjectBolt/Player/WeaponPivot.gd
@@ -9,15 +9,16 @@ func _physics_process(delta):
 	var space_state = get_world_3d().direct_space_state
 	var mouse_position = get_viewport().get_mouse_position()
 	var ray_query = PhysicsRayQueryParameters3D.new()
-	
+
 	rayOrigin = camera.project_ray_origin(mouse_position)
 	rayEnd = rayOrigin + camera.project_ray_normal(mouse_position) * 2000
 	ray_query.from = rayOrigin
 	ray_query.to = rayEnd
 	ray_query.collide_with_areas = true
 	var raycast_result = space_state.intersect_ray(ray_query)
-	
+
 	if not raycast_result.is_empty():
 		var pos = raycast_result.position
 		get_node("../gizmo").position = Vector3(pos.x, pos.y, 0)
 		look_at(Vector3(pos.x, pos.y, 0), Vector3(0,0,1))
+
--- ProjectBolt/Player/player.gd
+++ ProjectBolt/Player/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody3D
 
 @export var move_speed = 7.0
 @export var jump_strength = 5
+@export var thrust_strength = 14
+@export var fuel = 60
 @export var acceleration = 100.0
 @export var friction = 100.0
 
@@ -19,7 +21,7 @@ func _process(delta):
 		var sp = smallProjectile.instantiate()
 		owner.add_child(sp)
 		sp.transform = $WeaponPivot/ProjectileSpawn.global_transform
-		sp.velocity = -sp.transform.basis.x * sp.speed
+		sp.velocity = -sp.transform.basis.z * sp.speed
 
 	if Input.is_action_just_released("shoot"):
 		$ChargeTimer.stop()
@@ -32,28 +34,31 @@ func _process(delta):
 		var bp = bigProjectile.instantiate()
 		owner.add_child(bp)
 		bp.transform = $WeaponPivot/ProjectileSpawn.global_transform
-		bp.velocity = -bp.transform.basis.x * bp.speed
+		bp.velocity = -bp.transform.basis.z * bp.speed
 		
 		chargeStatus = 0
 		
+	print(fuel)
+		
 
 func _physics_process(delta):
-	# Add the gravity.
+
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+	
+	if is_on_floor() and fuel < 60:
+		fuel_reset()
 
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_strength
-
-
-#	var input_dir = Input.get_vector("left", "right", "up", "down")
-#	var direction = (transform.basis * Vector3(input_dir.x, 0, 0)).normalized()
-#	if direction:
-#		velocity.x = direction.x * move_speed
-#	else:
-#		velocity.x = move_toward(velocity.x, 0, move_speed)
-
+		
+	if Input.is_action_pressed("thrust") and fuel > 0:
+		velocity.y += thrust_strength * delta
+		fuel -= 1
+		
+	
+	
 	var input_vector = 0
 	input_vector =Input.get_action_strength("right") - Input.get_action_strength("left")
 	
@@ -62,8 +67,10 @@ func _physics_process(delta):
 	else:
 		velocity = Vector3(move_toward(velocity.x, 0, friction * delta),velocity.y, velocity.z)
 
-	print(velocity)
 	move_and_slide()
+	
+func fuel_reset():
+		fuel = fuel + 2
 
 
 func _on_charge_timer_timeout():
@@ -75,3 +82,4 @@ func _on_charge_timer_timeout():
 func _on_charge_up_timer_timeout():
 	print("CHARGEBEAM READY")
 	chargeStatus = 2
+
